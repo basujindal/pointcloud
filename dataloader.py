645caaf0a7162b10be3f019d493431031604a3b7
Basu Jindal <42815171+basujindal@users.noreply.github.com>
2021-05-28 14:05:41 +0530

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -1,26 +1,3 @@
-import h5py
-import os
-import random
-from torch.utils.data import Dataset
-import numpy as np
-import pickle
-import torch.nn as nn
-import math
-import torch
-from pytorch3d.loss import chamfer_distance
-from torch.utils.data import DataLoader
-from tqdm import tqdm
-import pytorch3d
-from torch.utils.data import Dataset
-import numpy as np
-import os
-import matplotlib.pyplot as plt
-import sys
-import torch.nn.functional as F
-import numpy as np
-import pandas as pd
-from sklearn.metrics.pairwise import cosine_distances
-
 class PCDataLoader(Dataset):
     def __init__(self, points, gt,labels, n_points=2048):
         self.n_points = n_points
